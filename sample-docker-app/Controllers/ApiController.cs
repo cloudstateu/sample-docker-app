e16b0d0cd0646f7b927387e701db7d9f7e3c0d68
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2021-02-08 14:06:54 +0100

    Template cleanup, added server and req info

---
+++ sample-docker-app/Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace sample_docker_app.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public object Get(string name = null)
+        {
+            return new
+            {
+                MachineName = Environment.MachineName,
+                Os = Environment.OSVersion.VersionString,
+                Request = $"{Request.Host.Value}{Request.Path}{Request.QueryString}"
+            };
+        }
+    }
+}
